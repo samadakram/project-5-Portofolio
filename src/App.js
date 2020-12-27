110c43adeb0db0098308296f49b1fcf2e7f4ccce
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-27 13:13:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Hello world</h1>
     </div>
   );
