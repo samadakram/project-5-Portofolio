c7a640beb00d8b0f5bffec392edebdb1014e4c47
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-28 16:12:15 +0500

    Update Skills.jsx

--- src/assests/components/Skills/Skills.jsx
+++ src/assests/components/Skills/Skills.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 import './Skills.css';
 
 export const Skills = () => {
     return (
         <div className="skillsContainer">
             <h1>Skills</h1>
+            <div>
+                <Grid container>
+                    <Grid item xs={12} md={2}>
+                        <i className="fab html fa-html5"></i>
+                        <p>HTML 5</p>
+                    </Grid>
+                    <Grid item xs={12} md={2}>
+                        <i className="fab css fa-css3"></i>
+                        <p>CSS 3</p>
+                    </Grid>
+                    <Grid item xs={12} md={2}>
+                        <i className="fab bootstrap fa-bootstrap"></i>
+                        <p>Bootstrap</p>
+                    </Grid>
+                    <Grid item xs={12} md={2}>
+                        <i className="fab javascript fa-js"></i>
+                        <p>Javascript</p>
+                    </Grid>
+                    <Grid item xs={12} md={2}>
+                        <i className="fab react fa-react"></i>
+                        <p>React</p>
+                    </Grid>
+                    <Grid item xs={12} md={2}>
+                        <i className="fab typescript fa-react"></i>
+                        <p>Typescript</p>
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     );
 }
