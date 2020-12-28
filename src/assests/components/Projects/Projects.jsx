522a984ba19451c5c7b0565e7d5f76564e2c3cc4
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-28 16:41:10 +0500

    Update Projects.jsx

--- src/assests/components/Projects/Projects.jsx
+++ src/assests/components/Projects/Projects.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Projects.css';
+import { Grid } from '@material-ui/core';
 
 export const Projects = () => {
-    return(
+    return (
         <div className="projectsContainer">
             <h1>Projects</h1>
+            <div className="projects">
+                <Grid container>
+                    <Grid item xs={12} md={6}>Project 1</Grid>
+                    <Grid item xs={12} md={6}>Project 2</Grid>
+                    <Grid item xs={12} md={6}>Project 3</Grid>
+                    <Grid item xs={12} md={6}>Project 4</Grid>
+                </Grid>
+            </div>
         </div>
     );
 }
