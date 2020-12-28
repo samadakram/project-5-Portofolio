bfacbe680e95d8441d57f51d12283b478c45ee94
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-28 13:16:30 +0500

    Update Home.jsx

--- src/assests/components/Home/Home.jsx
+++ src/assests/components/Home/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import {Grid} from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import './Home.css';
 
 export const Home = () => {
     return (
         <div>
             <Grid container>
-                <Grid className="homeGrid" item md={6}>Text</Grid>
+                <Grid className="homeGrid" item md={6}>
+                    <h4 className="welcome">WELCOME TO MY WORLD</h4>
+                    <h1 className="name">Hi, I'm Abdul Samad</h1>
+                    <h1 className="country">from Pakistan</h1>
+                </Grid>
                 <Grid className="homeGrid" item md={6}>Image</Grid>
-            </Grid>            
+            </Grid>
         </div>
     );
 }
