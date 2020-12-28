790527d2ba84238d495cc16dbda39f5dc2d300ea
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-28 17:40:53 +0500

    Update Contact.jsx

--- src/assests/components/Contact/Contact.jsx
+++ src/assests/components/Contact/Contact.jsx
@@ -8,10 +8,38 @@ export const Contact = () => {
         <div className="contactContainer">
             <Grid container>
                 <Grid item xs={12}>
-                    <img src={Profile} alt="Profile"/>
+                    <img src={Profile} alt="Profile" />
                 </Grid>
-                <Grid item xs={12}>Contact</Grid>
-                <Grid item xs={12}> Contac cards</Grid>
+                <Grid item xs={12}>
+                    <h3>Contact</h3>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <div className="contactCard">
+                        <div className="contactIcon">
+                            <i className="fas fa-envelope"></i>
+                        </div>
+                        <div className="contactEmail">
+                            <span>Email</span>
+                        </div>
+                        <div className="contactLink">
+                            <span>as765722@gmail.com</span>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <div className="contactCard">
+                        <div className="contactIcon">
+                            <i className="fas fa-envelope"></i>
+                        </div>
+                        <div className="contactEmail">
+                            <span>Email</span>
+                        </div>
+                        <div className="contactLink">
+                            <span>as765722@gmail.com</span>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={12} md={4}>Phone</Grid>
             </Grid>
         </div>
     );
