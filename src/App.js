fc3ce2fa117f94b49f0f72b1639bbd020ef0d45c
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-28 13:43:24 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.module.css';
 // Import Components
 import { NavBar } from './assests/components/NavBar/NavBar';
 import { Home } from './assests/components/Home/Home';
+import { Services } from './assests/components/Services/Services';
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
+      <Services />
     </div>
   );
 }
