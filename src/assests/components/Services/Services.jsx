511ea95ca2c2d545d45af008fbb876908e26a188
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-28 13:46:58 +0500

    Update Services.jsx

--- src/assests/components/Services/Services.jsx
+++ src/assests/components/Services/Services.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 
 export const Services = () => {
     return (
         <div>
             <h1>Services</h1>
+            <Grid container>
+                <Grid item xs={12} md={6}>Card 1</Grid>
+                <Grid item xs={12} md={6}>Card 2</Grid>
+            </Grid>
         </div>
     );
 }
