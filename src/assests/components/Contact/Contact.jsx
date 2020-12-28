a52d224e29206003aae09b48d7bbeefadfc00321
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-28 17:32:06 +0500

    Update Contact.jsx

--- src/assests/components/Contact/Contact.jsx
+++ src/assests/components/Contact/Contact.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Contact.css';
 import { Grid } from '@material-ui/core';
+import Profile from '../../images/ContactImages/Profile.jpg';
 
 export const Contact = () => {
     return (
         <div className="contactContainer">
             <Grid container>
-                <Grid item xs={12}>Photo</Grid>
+                <Grid item xs={12}>
+                    <img src={Profile} alt="Profile"/>
+                </Grid>
                 <Grid item xs={12}>Contact</Grid>
                 <Grid item xs={12}> Contac cards</Grid>
             </Grid>
