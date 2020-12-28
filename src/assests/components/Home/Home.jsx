62057a4f1cb7ad6f17e936478cc00e4e91662a8e
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-28 19:46:18 +0500

    Update Home.jsx

--- src/assests/components/Home/Home.jsx
+++ src/assests/components/Home/Home.jsx
@@ -5,15 +5,15 @@ import HomeImage from '../../../assests/images/HomeImages/selected3.svg';
 
 export const Home = () => {
     return (
-        <div>
+        <div className="homeContainer">
             <Grid container>
-                <Grid className="homeGrid" item md={6}>
+                <Grid className="homeGrid textGrid" item xs={12} md={6}>
                     <h4 className="welcome">WELCOME TO MY WORLD</h4>
                     <h1 className="name">Hi, I'm Abdul Samad</h1>
                     <h1 className="country">from Pakistan</h1>
-                    <button>Contact me</button>
+                    <button className="button">Contact me</button>
                 </Grid>
-                <Grid className="homeGrid" item md={6}>
+                <Grid className="homeGrid imageGrid" item xs={12} md={6}>
                     <img className="workingImage" src={HomeImage} alt="Working" />
                 </Grid>
             </Grid>
