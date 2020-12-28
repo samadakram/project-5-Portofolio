6f03cbb9add52492ad3335def06596c01c92a009
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-28 14:01:03 +0500

    Update Services.jsx

--- src/assests/components/Services/Services.jsx
+++ src/assests/components/Services/Services.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
-import WebImage from '../../images/ServicesImages/web.svg'; 
+import WebImage from '../../images/ServicesImages/web.svg';
+import ReactImage from '../../images/ServicesImages/react.svg';
 
 export const Services = () => {
     return (
@@ -8,14 +9,21 @@ export const Services = () => {
             <h1>Services</h1>
             <Grid container>
                 <Grid item xs={12} md={6}>
-                    <div>
+                    <div className="cards">
                         <li>
                             <img src={WebImage} alt="web"/>
                         </li>
                         <p>Web Development</p>
                     </div>
                 </Grid>
-                <Grid item xs={12} md={6}>Card 2</Grid>
+                <Grid item xs={12} md={6}>
+                <div className="cards">
+                        <li>
+                            <img src={ReactImage} alt="react"/>
+                        </li>
+                        <p>React Development</p>
+                    </div>
+                </Grid>
             </Grid>
         </div>
     );
