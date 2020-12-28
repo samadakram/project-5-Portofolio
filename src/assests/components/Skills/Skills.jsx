49bb89ccb29f4758021e25c0ef809322785bd1b0
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-28 16:14:29 +0500

    Update Skills.jsx

--- src/assests/components/Skills/Skills.jsx
+++ src/assests/components/Skills/Skills.jsx
@@ -8,27 +8,27 @@ export const Skills = () => {
             <h1>Skills</h1>
             <div>
                 <Grid container>
-                    <Grid item xs={12} md={2}>
+                    <Grid item xs={12} sm={4} md={2} className="skillItem">
                         <i className="fab html fa-html5"></i>
                         <p>HTML 5</p>
                     </Grid>
-                    <Grid item xs={12} md={2}>
+                    <Grid item xs={12} sm={4} md={2} className="skillItem">
                         <i className="fab css fa-css3"></i>
                         <p>CSS 3</p>
                     </Grid>
-                    <Grid item xs={12} md={2}>
+                    <Grid item xs={12} sm={4} md={2} className="skillItem">
                         <i className="fab bootstrap fa-bootstrap"></i>
                         <p>Bootstrap</p>
                     </Grid>
-                    <Grid item xs={12} md={2}>
+                    <Grid item xs={12} sm={4} md={2} className="skillItem">
                         <i className="fab javascript fa-js"></i>
                         <p>Javascript</p>
                     </Grid>
-                    <Grid item xs={12} md={2}>
+                    <Grid item xs={12} sm={4} md={2} className="skillItem">
                         <i className="fab react fa-react"></i>
                         <p>React</p>
                     </Grid>
-                    <Grid item xs={12} md={2}>
+                    <Grid item xs={12} sm={4} md={2} className="skillItem">
                         <i className="fab typescript fa-react"></i>
                         <p>Typescript</p>
                     </Grid>
