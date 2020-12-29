9eafbcd2e9ded8e4ebe4c0b5a435991734dc6c3b
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-29 14:43:02 +0500

    Update Home.jsx

--- src/assests/components/Home/Home.jsx
+++ src/assests/components/Home/Home.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import './Home.css';
 import HomeImage from '../../../assests/images/HomeImages/selected3.svg';
-import useWebAnimations from '@wellyshen/use-web-animations';
+// import useWebAnimations from '@wellyshen/use-web-animations';
 
 export const Home = () => {
 
-    const { ref } = useWebAnimations({
-        keyframes:[
-        { transform: "translateY(-20px)" },
-        { transform: "translateY(0)" }
-    ], timing:{
-        duration: 2000,
-        iterations: Infinity,
-        direction: "alternate",
-        easing: "ease-in-out",
-    }});
+    // const { ref } = useWebAnimations({
+    //     keyframes:[
+    //     { transform: "translateY(-20px)" },
+    //     { transform: "translateY(0)" }
+    // ], timing:{
+    //     duration: 2000,
+    //     iterations: Infinity,
+    //     direction: "alternate",
+    //     easing: "ease-in-out",
+    // }});
 
     return (
         <div className="homeContainer">
@@ -27,7 +27,7 @@ export const Home = () => {
                     <button className="button">Contact me</button>
                 </Grid>
                 <Grid className="homeGrid imageGrid" item xs={12} md={6}>
-                    <img ref={ref} className="workingImage" src={HomeImage} alt="Working" />
+                    <img  className="workingImage" src={HomeImage} alt="Working" />
                 </Grid>
             </Grid>
         </div>
