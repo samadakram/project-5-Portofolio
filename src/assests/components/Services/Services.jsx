0b5923cfa14545ee39ee4270a7e6d4c2fcc9b072
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-29 18:19:25 +0500

    Update Services.jsx

--- src/assests/components/Services/Services.jsx
+++ src/assests/components/Services/Services.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Services.css';
 import { Grid } from '@material-ui/core';
 import WebImage from '../../images/ServicesImages/web.svg';
 import ReactImage from '../../images/ServicesImages/react.svg';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 export const Services = () => {
+
+    useEffect(()=>{
+        Aos.init({duration:2000});
+    },[]);
+
     return (
-        <div className="servicesContainer" id="services">
+        <div className="servicesContainer" id="services" data-aos="zoom-in">
             <h1 className="serviceHeading">Services I Provide</h1>
             <Grid container>
-                <Grid item xs={12} md={6}  className="serviceGrid">
+                <Grid item xs={12} md={6} className="serviceGrid">
                     <div className="cards">
                         <li>
                             <img src={WebImage} alt="web" />
