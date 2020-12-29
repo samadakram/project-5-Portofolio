42bc06dbfedd6d5a698a83f715cde643871f685e
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-29 13:30:54 +0500

    Update Footer.jsx

--- src/assests/components/Footer/Footer.jsx
+++ src/assests/components/Footer/Footer.jsx
@@ -4,8 +4,8 @@ import './Footer.css';
 export const Footer = () => {
     return (
         <div className="footerContainer">
-            <h2>Developer :
-            <a href="https://github.com/samadakram" rel="noopener noreferrer" target="_blank">Abdul Samad</a>
+            <h2>Developer : 
+            <a href="https://github.com/samadakram" rel="noopener noreferrer" target="_blank"> Abdul Samad</a>
             </h2>
         </div>
     );
